ff8f4dda7253fe888ac8fb60c2de0f258d7f6d62
kianweelee <leekianwee@outlook.com>
2020-05-16 15:41:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     long_description=open('README.txt').read(),
     install_requires=['os', 'pandas', 'matplotlib', 'seaborn', 'PySimpleGUI', 'sklearn', 'numpy', 'scipy', 'statsmodels', 'itertools'],
     url='https://https://github.com/kianweelee/Edator',
+    download_url= 'https://github.com/kianweelee/Edator/archive/0.1.tar.gz',
     author='Lee Kian Wee',
     author_email='leekianwee@outlook.com'
 )
